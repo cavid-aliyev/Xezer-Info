eb7ab05364edd5c3c0a74094899b26c11f918e70
Alyeev <markmarkul123@gmail.com>
2021-08-31 18:36:35 +0400

    Add login 847 register

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const app = express();
-const doteenv = require("dotenv")
-const mongoose = require("mongoose")
+const doteenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
+//router details
+app.use(express.json());
+app.use("/api/auth", authRoute);
 
 doteenv.config();
+const PORT = 5000;
 
-
-mongoose.connect(process.env.MONGO_URL , {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(console.log("Mongoose connected")).catch(err => console.log(err))
+  })
+  .then(console.log("Mongoose connected"))
+  .catch((err) => console.log(err));
 
-
-const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Backend is running in on ${PORT} port`);
 });
---
+++ server/routes/auth.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+
+//grab user model from models
+const User = require("../models/User");
+
+//!REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    //hashed pass
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+
+//!LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    //find user from mongo
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong credentials!");
+
+    //compare pass
+    const validate = await bcrypt.compare(req.body.password, user.password);
+    !validate && res.status(400).json("Passwords are not compared!");
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
