dbe6d8ce0f72d5387525134299affff031ca64d3
Alyeev <markmarkul123@gmail.com>
2021-09-01 14:30:43 +0400

    Add register system

--- client/src/pages/Registerpage.jsx
+++ client/src/pages/Registerpage.jsx
@@ -1,31 +1,58 @@
 import React from "react";
+import axios from "axios";
 
 const Registerpage = () => {
+  const [username, setUsername] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false)
+    try {
+      const res = await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      res.data && window.location.replace("/login");
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
     <div className="register">
       <span className="registerTitle">Qeydiyyatdan keç</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Ad</label>
         <input
           className="registerInput"
           type="text"
           placeholder="Enter your username..."
+          onChange={(e) => setUsername(e.target.value)}
         />
         <label>Email</label>
         <input
           className="registerInput"
           type="text"
           placeholder="Enter your email..."
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label>Şifrə</label>
         <input
           className="registerInput"
           type="password"
           placeholder="Enter your password..."
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="registerButton">Qeydiyyatdan keç</button>
+        <button className="registerButton" type="submit">
+          Qeydiyyatdan keç
+        </button>
       </form>
       <button className="registerLoginButton">Daxil ol</button>
+      {error && <span>Xəta baş verdi</span>}
     </div>
   );
 };
